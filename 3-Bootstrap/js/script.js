df7e86f99bfbf78cd32af7419aef692ecf0a5f04
sondossamii <sondos.sami.abuzeid@gmail.com>
2022-05-08 23:32:45 +0200

    Add 'Clients' and use 'WOW' Package

--- 3-Bootstrap/js/script.js
+++ 3-Bootstrap/js/script.js
@@ -1,9 +1,12 @@
 $(document).ready(function () {
   // $(".loader").style.display = "none";
-  $(".spinner").fadeOut(2000, function () {
-    $("body").css("overflow", "auto");
-    $(this).parent().fadeOut(1000);
-    $(this).parent().remove();
+  // $("html").animate({ scrollTop: 0 }, 100);
+  $("html").animate({ scrollTop: 0 }, function () {
+    $(".spinner").fadeOut(2000, function () {
+      $("body").css("overflow", "auto");
+      $(this).parent().fadeOut(1000);
+      $(this).parent().remove();
+    });
   });
 });
 
